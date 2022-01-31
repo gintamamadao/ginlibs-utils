c56732d325d421165844adab8586e36711c794b7
gintamamadao <gintamaginsan@126.com>
2022-01-31 14:49:12 +0800

    feat: build config

--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,26 @@ import typescript from 'rollup-plugin-typescript2'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import babel from '@rollup/plugin-babel'
+import pkg from './package.json'
 import json from '@rollup/plugin-json'
 import addDts from 'rollup-plugin-add-global-ts'
-import pkg from './package.json'
-
-const extensions = ['.js', '.ts']
 
 const babelOpt = {
-  extensions,
+  extensions: ['.js', '.ts'],
   babelHelpers: 'runtime',
   exclude: 'node_modules/**',
   include: ['src/**/*'],
   plugins: ['@babel/plugin-transform-runtime'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: '10',
+        },
+      },
+    ],
+  ],
 }
 
 export default [
